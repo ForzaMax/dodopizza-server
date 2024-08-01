808a1e29d63644df2ca29910e1ab72d6b7ef5df6
Maxim <fforzamax@gmail.com>
2024-08-01 14:21:16 +0300

    feat: add dotenv package

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require("express"); // Подключаем модуль express для работы с HTTP-запросами 
+const express = require("express"); // Подключаем модуль express для работы с HTTP-запросами
 const mongoose = require("mongoose"); // Подключаем модуль mongoose для работы с MongoDB
-const cors = require("cors"); // Подключаем модуль cors для работы с CORS запросами 
-const path = require("path"); // Подключаем модуль path для работы с путями  
+const cors = require("cors"); // Подключаем модуль cors для работы с CORS запросами
+const path = require("path"); // Подключаем модуль path для работы с путями
+const dotenv = require("dotenv").config(); // Подключаем модуль dotenv для работы с переменными окружения
 
 const app = express(); // Инициализируем приложение
 
-app.use(cors());  // Включаем поддержку CORS для всех маршрутов
+app.use(cors()); // Включаем поддержку CORS для всех маршрутов
 app.use("/public", express.static(path.join(__dirname, "public"))); // Добавляем путь к папке public. Эта строка определяет статическую директорию /public, из которой будут обслуживаться статические файлы (изображения).
 
 mongoose
@@ -17,9 +18,9 @@ mongoose
 
 const productRoutes = require("./routes/products"); // Добавляем маршруты продуктов
 const statisticsRoutes = require("./routes/statistics"); // Добавляем маршруты статистики
- 
-app.use("/products", productRoutes);   
-app.use("/statistics", statisticsRoutes); 
+
+app.use("/products", productRoutes);
+app.use("/statistics", statisticsRoutes);
 
 const PORT = process.env.PORT || 5000; // Порт сервера
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
